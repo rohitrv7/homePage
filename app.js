6a2467cc0620c0afd6f4d0b8126d364637b1195a
rohitrv7 <rohitkumar115032002@gmail.com>
2024-05-31 00:07:33 +0530

    ad home and nav animation

--- app.js
+++ app.js
@@ -1,22 +1,79 @@
-let menu = document.querySelector(".menu");
-let link = document.querySelector(".linkMobile");
-let close = document.querySelector(".close");
+(() => {
+  let menu = document.querySelector(".menu");
+  let link = document.querySelector(".linkMobile");
+  let close = document.querySelector(".close");
 
-let tl = gsap.timeline();
+  let tl = gsap.timeline();
 
-function navbar() {
-  tl.to(link, {
-    right: "-120",
-    duration: 0.5,
+  function navbar() {
+    tl.to(link, {
+      right: "-120",
+      duration: 0.5,
+    });
+
+    tl.pause();
+    menu.addEventListener("click", (e) => {
+      tl.play();
+    });
+
+    link.addEventListener("click", (e) => {
+      tl.reverse();
+    });
+  }
+  navbar();
+})();
+
+function nav() {
+  let tl = gsap.timeline();
+  tl.from("nav .logo", {
+    y: -50,
+    opacity: 0,
+    duration: 0.4,
   });
-
-  tl.pause();
-  menu.addEventListener("click", (e) => {
-    tl.play();
-  });
-
-  link.addEventListener("click", (e) => {
-    tl.reverse();
+  tl.from(".link a", {
+    y: -50,
+    opacity: 0,
+    stagger: 0.2,
+    duration: 0.3,
   });
 }
-navbar();
+nav();
+
+function home() {
+  let tl = gsap.timeline();
+  tl.from(".home h1", {
+    x: -50,
+    opacity: 0,
+    duration: 0.8,
+    stagger: 0.3,
+  });
+  tl.from(".home p", {
+    x: -50,
+    opacity: 0,
+    duration: 0.4,
+  });
+  tl.from(".content button", {
+    y: 50,
+    opacity: 0,
+    duration: 0.2,
+  });
+}
+home();
+
+
+// let tl = gsap.timeline()
+// tl.from(".aboutimg img",{
+//     x: -100,
+//     opacity: 0,
+//     duration:0.5,
+//     scrollTrigger:{
+//         trigger:".aboutPage",
+//         start:"top center",
+//         end: "-100%",
+//         marker: true,
+//         scrub: true
+//     }
+// },"a")
+// tl.from(".aboutPage h1",{
+
+// })
